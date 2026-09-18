5031795dbb6d70daf4c65a2426a97f5d41d536cf
luisteixeira-alt <luis.teixeira@eaportal.org>
2026-09-18 10:41:55 +0000

    projeto final

---
+++ atividades/revisao.py
@@ -0,0 +1,8 @@
+class Veiculo:
+    def __init__(self, marca, modelo, valor_diaria):
+        self.marca = marca
+        self.modelo = modelo
+        self.__valor_diaria = valor_diaria
+    def get_valor_diaria = valor_diaria
+        return self.__valor_diaria
+        
